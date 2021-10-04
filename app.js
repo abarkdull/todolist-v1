28213b1a02063464734f74797a3335ea2575b94b
abarkdull <abarkdull@gmail.com>
2021-10-04 16:52:09 -0600

    add express utilities to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const { urlencoded } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended = true}));
+
+
+
+app.listen(3000, function() {
+    console.log('Server running on port 3000 at https://localhost:3000');
+})
