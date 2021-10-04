f75f191f7211aed3ff149b90a7bced0313eef064
abarkdull <abarkdull@gmail.com>
2021-10-04 17:19:03 -0600

    add ejs

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.use(express.urlencoded({ extended = true}));
 
+app.set('view engine', 'ejs');
 
+app.get("/", function(req, res) {
+
+});
 
 app.listen(3000, function() {
     console.log('Server running on port 3000 at https://localhost:3000');
-})
+});
