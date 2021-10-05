693485b27aa23ecbcf1d0cad1e2ae32ec4f4d923
abarkdull <abarkdull@gmail.com>
2021-10-05 11:10:05 -0600

    modify app.js

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({ extended = true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
+    var date = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = date.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
+
+app.post("/", function(req, res) {
+
+    console.log(req.body.newItem);
+    // var toDoList = [];
+
+    // var newItem = req.body.newItem;
+    // toDoList.push(newItem);
+
+    // res.render("list", {
+    //     toDos: toDoList,
+    //     kindOfDay: "hello"
+    // })
+})
 
 app.listen(3000, function() {
     console.log('Server running on port 3000 at https://localhost:3000');
